d4bb64509acf36f555fe11b7b586e4adf3202925
speeaD <sejiofor20@gmail.com>
2024-11-23 08:20:24 +0100

    update

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,7 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
+
 export const load = (async ({ locals }) => {
 	try {
 		const [showcaseRecords, carouselsRecords] = await Promise.all([
